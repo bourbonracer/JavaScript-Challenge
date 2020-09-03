7755b583bb2465d3e34432b1747f22144c49240e
bourbonracer <bourbonracer@gmail.com>
2020-09-03 16:33:07 -0700

    Added directory, starter code and append rows and fill with data in app.js

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,15 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+// APPEND ROWS AND FILL WITH DATA
+tableData.forEach((ufoSighting) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
