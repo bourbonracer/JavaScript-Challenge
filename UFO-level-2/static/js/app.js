c4363602d10b6d753bf52eb93068ebdd66c10e47
bourbonracer <bourbonracer@gmail.com>
2020-09-03 22:41:02 -0700

    Added shop to function. Works!

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -5,7 +5,7 @@ var tableData = data;
 
 var tbody = d3.select("tbody");
 
-// APPEND ROWS AND FILL WITH DATA
+// Append rows and fill with data
 tableData.forEach((ufoSighting) => {
     var row = tbody.append("tr");
     Object.entries(ufoSighting).forEach(function([key, value]) {
@@ -14,30 +14,33 @@ tableData.forEach((ufoSighting) => {
     });
 });
 
-// SELECT THE FORM AND BUTTON
+// Select the form and button
 var form = d3.select("#form");
 var button = d3.select("#filter-btn");
 
-// HANDLERS FOR CLICKING BUTTON OR PRESSING ENTER KEY
+// Handles for click and submit
 button.on("click", runEnter);
 form.on("submit", runEnter);
 
-//FUNCTION TO RUN BOTH EVENTS WITH FILTERS FOR DATE, CITY, STATE, COUNTRY AND SHAPE
+// Function to run both events with filters for date, city, state, country and shop
 function runEnter() {
     d3.event.preventDefault();
     var inputDate = d3.select("#datetime");
     var inputCity = d3.select("#city");
     var inputState = d3.select("#state");
     var inputCountry = d3.select("#country");
+    var inputShape = d3.select("#shape");
     var dateValue = inputDate.property("value");
     var cityValue = inputCity.property("value");
     var stateValue = inputState.property("value");
     var countryValue = inputCountry.property("value");
+    var shapeValue = inputShape.property("value");
     tbody.html("");
     var filteredDate = tableData.filter(dateData => dateData.datetime === dateValue);
     var filteredCity = tableData.filter(cityData => cityData.city === cityValue);
     var filteredState = tableData.filter(stateData => stateData.state === stateValue);
     var filteredCountry = tableData.filter(countryData => countryData.country === countryValue);
+    var filteredShape = tableData.filter(shapeData => shapeData.shape === shapeValue);
     filteredDate.forEach((FilterDate) => {
         var row = tbody.append("tr");
         Object.entries(FilterDate).forEach(function([key, value]) {
@@ -66,57 +69,11 @@ function runEnter() {
             cell.text(value);
         });
     });
+    filteredShape.forEach((FilterShape) => {
+        var row = tbody.append("tr");
+        Object.entries(FilterShape).forEach(function([key, value]) {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
 };
-
-// function runCity() {
-//     d3.event.preventDefault();
-//     var inputCity = d3.select("#city");
-//     var cityValue = inputCity.property("value");
-//     tbody.html("");
-//     var filteredCity = tableData.filter(cityData => cityData.city === cityValue);
-//     filteredCity.forEach((FilterCity) => {
-//         var row = tbody.append("tr");
-//         Object.entries(FilterCity).forEach(function([key, value]) {
-//             var cell = row.append("td");
-//             cell.text(value);
-//         });
-//     });
-// };
-
-//     d3.event.preventDefault();
-//     var inputState = d3.select("#state");
-//     var stateValue = inputState.property("value");
-//     tbody.html("");
-//     var filteredState = tableData.filter(stateData => stateData.state === stateValue);
-//     filteredState.forEach((FilterState) => {
-//         var row = tbody.append("tr");
-//         Object.entries(FilterState).forEach(function([key, value]) {
-//             var cell = row.append("td");
-//             cell.text(value);
-//         });
-//     });
-//     d3.event.preventDefault();
-//     tbody.html("");
-//     var inputCountry = d3.select("#country");
-//     var countryValue = inputCountry.property("value");
-//     var filteredCountry = tableData.filter(countryData => countryData.country === countryValue);
-//     filteredCountry.forEach((FilterCountry) => {
-//         var row = tbody.append("tr");
-//         Object.entries(FilterCountry).forEach(function([key, value]) {
-//             var cell = row.append("td");
-//             cell.text(value);
-//         });
-//     });
-//     d3.event.preventDefault();
-//     var inputShape = d3.select("#shape");
-//     var shapeValue = inputShape.property("value");
-//     tbody.html("");
-//     var filteredShape = tableData.filter(shapeData => shapeData.shape === shapeValue);
-//     filteredShape.forEach((FilterShape) => {
-//         var row = tbody.append("tr");
-//         Object.entries(FilterShape).forEach(function([key, value]) {
-//             var cell = row.append("td");
-//             cell.text(value);
-//         });
-//     });
-// };
