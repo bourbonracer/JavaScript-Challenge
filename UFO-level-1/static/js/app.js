51d853303d0c8eb64f68080667ea72df43aee3ec
bourbonracer <bourbonracer@gmail.com>
2020-09-03 19:56:43 -0700

    Added function runEnter.  Code runs

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -14,12 +14,25 @@ tableData.forEach((ufoSighting) => {
 });
 
 // SELECT THE FORM AND BUTTON
-var form = d3.select("form");
+var form = d3.select("#form");
 var button = d3.select("#filter-btn");
 
 // HANDLERS FOR CLICKING BUTTON OR PRESSING ENTER KEY
 button.on("click", runEnter);
 form.on("submit", runEnter);
 
-
-}
+//FUNCTION TO RUN BOTH EVENTS
+function runEnter() {
+    d3.event.preventDefault();
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+    tbody.html("");
+    var filteredData = tableData.filter(dateData => dateData.datetime === inputValue);
+    filteredData.forEach((FilterSighting) => {
+        var row = tbody.append("tr");
+        Object.entries(FilterSighting).forEach(function([key, value]) {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+};
